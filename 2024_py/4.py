a506ef2ed3bc2d4af25d957a15400a75bb2ba4c6
MP <panchenko.maxim87@gmail.com>
2024-02-27 01:08:44 +0200

    2024_py 3-4task

---
+++ 2024_py/4.py
@@ -0,0 +1,22 @@
+"""
+Task
+Given an integral number, determine if it's a square number:
+
+In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+Examples
+-1  =>  false
+ 0  =>  true
+ 3  =>  false
+ 4  =>  true
+25  =>  true
+26  =>  false
+"""
+
+import math
+
+
+def is_square(n):
+    return n > -1 and math.isqrt(n)**2 == n
