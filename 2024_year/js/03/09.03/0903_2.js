af72858d9808dd17abf078638735c6760803273a
MP <panchenko.maxim87@gmail.com>
2024-03-09 23:39:22 +0200

    2024_js 0903_2task

---
+++ 2024_year/js/03/09.03/0903_2.js
@@ -0,0 +1,28 @@
+/**
+There is an array with some numbers. All numbers are equal except for one. 
+Try to find it!
+
+findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+It’s guaranteed that array contains at least 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.
+
+This is the first kata in series:
+
+Find the unique number (this kata)
+Find the unique string
+Find The Unique
+ */
+
+
+function findUniq(arr) {
+    for (let i=1; i != arr.length-1; i++){
+        if (arr[i-1]!=arr[i] && arr[i-1]!=arr[i+1]) return arr[i-1]
+        else if (arr[i]!=arr[i-1] && arr[i]!=arr[i+1]) return arr[i]
+        else if (arr[i+1]!=arr[i] && arr[i+1]!=arr[i-1]) return arr[i+1]
+    }
+}
+
+
+findUniq([0, 0, 0.55, 0, 0])
