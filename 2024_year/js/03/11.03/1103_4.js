53b57c462511b5cd70198a760017ef1a1db21f21
MP <panchenko.maxim87@gmail.com>
2024-03-11 21:39:08 +0200

    2024_js 1103_5task

---
+++ 2024_year/js/03/11.03/1103_4.js
@@ -0,0 +1,23 @@
+/**
+You are given an odd-length array of integers, in which all 
+of them are the same, except for one single number.
+
+Complete the method which accepts such an array, and returns 
+that single different number.
+
+The input array will always be valid! (odd-length >= 3)
+
+Examples
+[1, 1, 2] ==> 2
+[17, 17, 3, 17, 17, 17, 17] ==> 3
+ */
+
+
+function stray(numbers) {
+    let max_ = Math.max(...numbers);
+    let min_ = Math.min(...numbers);
+    return numbers.filter(x => x == max_).length == 1 ? max_ : min_;
+  }
+
+
+console.log(stray([17, 17, 3, 17, 17, 17, 17]))
