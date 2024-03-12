a6417c86b38847ed61fe4a796d38ce2b4fa85573
MP <panchenko.maxim87@gmail.com>
2024-03-12 23:02:35 +0200

    2024_js 1203_2task

---
+++ 2024_year/js/03/12.03/1203_2.js
@@ -0,0 +1,31 @@
+/**
+There is a bus moving in the city which takes and drops 
+some people at each bus stop.
+
+You are provided with a list (or array) of integer pairs. 
+Elements of each pair represent the number of people that 
+get on the bus (the first item) and the number of people that 
+get off the bus (the second item) at a bus stop.
+
+Your task is to return the number of people who are still 
+on the bus after the last bus stop (after the last array). 
+Even though it is the last bus stop, the bus might not be 
+empty and some people might still be inside the bus, they are probably sleeping there :D
+
+Take a look on the test cases.
+
+Please keep in mind that the test cases ensure that the number of people in 
+the bus is always >= 0. So the returned integer can't be negative.
+
+The second value in the first pair in the array is 0, since the bus is 
+empty in the first bus stop.
+ */
+
+
+var number = function(busStops){
+    let people_in_bus = busStops[0][0] - busStops[0][1];
+    for (let i = 1; i != busStops.length;i++){
+        people_in_bus = people_in_bus + busStops[i][0] - busStops[i][1] ;
+    }
+    return people_in_bus;
+  }
