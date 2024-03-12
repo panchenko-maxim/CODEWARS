d4136eebb98bb400d7f218c8e5d65287fe73c6a3
MP <panchenko.maxim87@gmail.com>
2024-03-12 22:40:24 +0200

    2024_py 1203_3-6task

---
+++ 2024_year/python/03/12.03/1203_6.py
@@ -0,0 +1,30 @@
+"""
+Task:
+Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+Rules:
+You need to round the answer to 2 decimal places and return it as String.
+
+If the given value is 0 then it should return 0.00
+
+You will only be given Natural Numbers as arguments.
+
+Examples:(Input --> Output)
+1 --> 1 --> "1.00"
+2 --> 1 + 1/4 --> "1.25"
+5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+"""
+
+
+def series_sum(n):
+    if n == 0:
+        return "0.00"
+    
+    result = 0
+    denominator = 1
+
+    for i in range(n):
+        result += 1 / denominator
+        denominator += 3
+    return "{:.2f}".format(result)
