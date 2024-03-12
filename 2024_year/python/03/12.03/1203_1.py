59cad67d12ec70c1db678e7f3e554f70bb5c26bb
MP <panchenko.maxim87@gmail.com>
2024-03-12 22:16:55 +0200

    2024_py 1203_1task

---
+++ 2024_year/python/03/12.03/1203_1.py
@@ -0,0 +1,31 @@
+"""
+Task
+You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+Examples
+[7, 1]  =>  [1, 7]
+[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+"""
+
+
+def sort_array(source_array):
+    source_array_copy = source_array.copy()
+    lst = []
+    indx = 0
+    for el in source_array:
+        if el % 2 == 1:
+            lst.append(el)
+    lst = sorted(lst)
+    
+    for i in range(len(source_array)):
+        if source_array[i] % 2 == 1:
+            source_array_copy[i] = lst[indx]
+            indx += 1
+    return source_array_copy
+
+
+sort_array([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
+
+
+     
