3ed1ba26c0942d53149019538c34deb3800775b3
MP <panchenko.maxim87@gmail.com>
2024-03-15 14:25:59 +0200

    2024_js 1503_1task

---
+++ 2024_year/js/03/15.03/1503_1.js
@@ -0,0 +1,11 @@
+/**
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+ */
+
+
+function findAverage(array) {
+    if (array.length == 0) return 0
+    return array.reduce((count, val)=> count + val, 0) / array.length
+  }
