72e8437694f527969b7604a52c88e0f83e6eb33d
MP <panchenko.maxim87@gmail.com>
2024-03-17 09:04:50 +0200

    2024_js 1703_3task

---
+++ 2024_year/js/03/17.03/1703_3.js
@@ -0,0 +1,29 @@
+/**
+Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+
+Examples:
+
+input:    output:
+0    ->   0
+2    ->   5
+3    ->   5
+12   ->   15
+21   ->   25
+30   ->   30
+-2   ->   0
+-5   ->   -5
+etc.
+Input may be any positive or negative integer (including 0).
+
+You can assume that all inputs are valid integers.
+ */
+
+
+function roundToNext5(n){
+    let k = n
+    while (k % 5 != 0) k += 1;
+    return k;
+  }
+
+
+roundToNext5(30)
