ed6a39bac86d87ab7d87bd6db07e287e8ffab4ce
MP <panchenko.maxim87@gmail.com>
2024-03-22 23:26:26 +0200

    2024_js 2203_1task

---
+++ 2024_year/js/03/22.03/2203_1.js
@@ -0,0 +1,17 @@
+/**
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+65 --> 'A'
+97 --> 'a'
+48 --> '0
+ */
+
+
+function getChar(c){
+   return String.fromCharCode(c)
+  }
+
+
+console.log(getChar(65))
