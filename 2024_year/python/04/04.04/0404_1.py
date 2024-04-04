2fb99b2f79a27574048ba84054100192be46f1e3
MP <panchenko.maxim87@gmail.com>
2024-04-04 22:53:38 +0300

    2024_py 0404_1task

---
+++ 2024_year/python/04/04.04/0404_1.py
@@ -0,0 +1,26 @@
+"""
+Your Job
+Find the sum of all multiples of n below m
+
+Keep in Mind
+n and m are natural numbers (positive integers)
+m is excluded from the multiples
+Examples
+sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+sumMul(4, -7)  ==> "INVALID"
+"""
+
+
+def sum_mul(n, m):
+    if m <= 0 or n <= 0: 
+        return 'INVALID'
+    elif n == m:
+        return 0
+    count = 0
+    for i in range(n, m, n):
+        count += i
+    return count
+
+print(sum_mul(0, 0))
